08259aaa3b3b9c3e14b0b709daa2eef64231ae22
Orient1314 <547191555@qq.com>
2021-01-12 21:05:06 +0800

    2020/1/12 21:05

---
+++ c-data-type/1.1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	printf("Ddta Type          number of bytes\n");
+	printf("-----------------------------------\n");
+	printf("char                      %d\n",sizeof(char));
+	printf("short                     %d\n",sizeof(short));
+	printf("int                      %d\n",sizeof(int));
+	printf("long                     %d\n",sizeof(long));
+	printf("float                      %d\n",sizeof(float));
+	printf("double                     %d\n",sizeof(double));
+	return 0;
+}
